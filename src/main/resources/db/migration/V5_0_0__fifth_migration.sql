e05ae690d8eefc778efcc3bf587cf8a545ccbab8
ub61555 <bhatiyaudit@johndeere.com>
2022-08-04 16:26:55 +0530

    New versions added

---
+++ src/main/resources/db/migration/V5_0_0__fifth_migration.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE hello (
+    id NUMERIC,
+    name VARCHAR(200)
+);
+
+insert into  hello VALUES (1, 'udit');
---
+++ src/main/resources/db/migration/V6_0_0__sixth_migration.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE new_table (
+    id NUMERIC,
+    name VARCHAR(200),
+    created_date DATE
+);
+
+insert into  new_table VALUES (1, 'udit', current_date);
